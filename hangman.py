df57e90285d1d984265de07c45a3a876a9c49599
david <dspiller321@gmail.com>
2026-05-13 12:13:48 +0300

    add vladichtion funchtion

--- hangman.py
+++ hangman.py
@@ -4,6 +4,26 @@ def choose_secret_word():# רשימת המילים ובחירת המילה
         "Window", "Door", "Wall", "Table", "Chair", "Notebook", "Pen", "Pencil", "Bag", "Book"]
     word=random.choice(words)
     return word
-def 
+def init_state(secret:str,max_tries:int):#יוצר מצב משחק
+    lst_str="_" .split * len(secret)
+    my_set=set()
+    dic={"secret":secret,
+         "display":lst_str,
+         "guessed":my_set,
+         "wrong_guesses":0,
+         "max_tries":max_tries}
+    return dic
+def prompt_guess():
+    letter=input("Enter a leeter please")
+    return letter
+def validate_guess(ch:str,gussed:set[str]):#אחראי על הבדיקת ולידציה
+    if (not ch.isalpha()) or (len(ch)>1) or (ch in gussed):
+        return False,"you need to choice one letter"
+    else:
+        return True,"your choice is valid"
+
+    
+
+
 
     
