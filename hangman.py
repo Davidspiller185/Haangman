fe987b8e02da16fd996c1955437d9c2c6e8bbf69
david <dspiller321@gmail.com>
2026-05-13 11:20:18 +0300

    create two function with the secret word and the state of the game

---
+++ hangman.py
@@ -0,0 +1,9 @@
+import random
+def choose_secret_word():# רשימת המילים ובחירת המילה
+    words=["Banana", "Apple", "Eggplant", "Tomato", "Cucumber", "Computer", "Mouse", "Keyboard", "Monitor", "Phone",
+        "Window", "Door", "Wall", "Table", "Chair", "Notebook", "Pen", "Pencil", "Bag", "Book"]
+    word=random.choice(words)
+    return word
+def 
+
+    
