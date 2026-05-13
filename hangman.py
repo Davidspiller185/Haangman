3495fa6ddc621741fc61e4e6d181669e4879d406
david <dspiller321@gmail.com>
2026-05-13 17:30:52 +0300

    code prefrorm

--- hangman.py
+++ hangman.py
@@ -1,28 +1,37 @@
 import random
-def choose_secret_word():# רשימת המילים ובחירת המילה
-    words=["Banana", "Apple", "Eggplant", "Tomato", "Cucumber", "Computer", "Mouse", "Keyboard", "Monitor", "Phone",
-        "Window", "Door", "Wall", "Table", "Chair", "Notebook", "Pen", "Pencil", "Bag", "Book"]
+def lst_words():#רשימת המילים
+     words=["banana", "apple", "eggplant", "tomato", "cucumber", "computer", "mouse", "keyboard", "monitor", "phone",
+        "window", "door", "wall", "table", "chair", "notebook", "pen", "pencil", "bag", "book"]
+     return words
+    
+def choose_secret_word(words:list):#  בחירת המילה
     word=random.choice(words)
     return word
 def init_state(secret:str,max_tries:int):#יוצר מצב משחק
-    lst_str="_" .split * len(secret)
-    my_set=set()
+    lst_str="_" .split() * len(secret)
+    my_guess=[]
     dic={"secret":secret,
          "display":lst_str,
-         "guessed":my_set,
+         "guessed":my_guess,
          "wrong_guesses":0,
          "max_tries":max_tries}
     return dic
 def prompt_guess():
-    letter=input("Enter a leeter please")
+    letter=input(" Enter a leeter please ")
     return letter
-def validate_guess(ch:str,gussed:set[str]):#אחראי על הבדיקת ולידציה
-    if (not ch.isalpha()) or (len(ch)>1) or (ch in gussed):
+def validate_guess(ch:str,gussed:list[str]):#אחראי על הבדיקת ולידציה
+    if (not ch.isalpha()) or (len(ch)>1) :
         return False,"you need to choice one letter"
+    elif ch in gussed:
+        return False,"the letter already choice"
+    elif not ch.isascii():
+        return False,"the leeter need to be in englich"
+    elif ch !=ch.lower():
+        return False,"the leeter need to be lower"
     else:
         return True,"your choice is valid"
 def apply_guess(state:dict,ch:str):
-    state["guesses"].add(ch)
+    state["guessed"].append((ch))
     flag=False
     for i in range(len(state["secret"])):
         if state["secret"][i]==ch:
@@ -37,7 +46,7 @@ def print_status(state:dict):
     remaining_guesses=state["max_tries"] - state["wrong_guesses"]
     print(f"the display is:{state["display"]},the guessed is:{state["guessed"]},the remaining_guesses:{remaining_guesses}")
 def is_won(state:dict):
-    if state["display"]==state["secret"]:
+    if "".join(state["display"])==state["secret"]:
         return True
     else:
         return False
@@ -53,9 +62,30 @@ def print_result(state:dict):
     elif is_lost(state):
         print("you lost the game because your max tries is finish")
         print(f"yhe word is {state["secret"]},and your guessed letter was:{state["guessed"]}")
-def play(words:list[str],max_tries:int=6):
+def main(words:list[str],max_tries:int=6):
     word=choose_secret_word(words)
-    dic=init_state(word,max_tries)       
+    dic=init_state(word,max_tries)
+    while dic["max_tries"]-dic["wrong_guesses"]>0 and "".join(dic["display"])!=dic["secret"]:
+        print_status(dic)
+        letter=prompt_guess()
+        tup_valid=validate_guess(letter,dic["guessed"])
+        while not tup_valid[0]:
+            print(tup_valid[1])
+            letter=prompt_guess()
+            tup_valid=validate_guess(letter,dic["guessed"])
+        print(tup_valid[1])
+        result_guesses=apply_guess(dic,letter)
+        if result_guesses:
+            print({"successes": "the letter is in the word"})
+        else:
+            print({"failed": "the letter is not in the word"})
+    print_result(dic)
+
+if __name__=="__main__":
+      words=lst_words()
+      main(words,5)
+
+
 
 
 
